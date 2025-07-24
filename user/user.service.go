8e2065ff6f94f3a72c1a9b4d6a6ad221b293b3e1
Onebluesky882 <onebluesky882@outlook.com>
2025-07-24 09:19:06 +0700

    update service

--- user/user.service.go
+++ user/user.service.go
@@ -17,6 +17,9 @@ func NewUserService(db *gorm.DB) *UserService {
 	return &UserService{DB: db}
 }
 
+// func แยก รับ s ของ *UserService
+// build-in method FindAll()
+// func return []User , error
 func (s *UserService) FindAll() ([]User, error) {
 	var users []User
 	err := s.DB.Find(&users).Error
@@ -27,6 +30,7 @@ func (s *UserService) Create(input *User) error {
 	if input.ID == uuid.Nil {
 		input.ID = uuid.New()
 	}
+	log.Println("Input")
 	err := s.DB.Create(input).Error
 	if err != nil {
 		log.Printf("create user error : %v", err)
