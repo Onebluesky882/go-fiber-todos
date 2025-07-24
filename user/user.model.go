1dea0a4bab1445fec575279d74cdc89010c85765
Onebluesky882 <onebluesky882@outlook.com>
2025-07-24 08:25:39 +0700

    fixed uuid insert

--- user/user.model.go
+++ user/user.model.go
@@ -7,7 +7,7 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `json:"id" gorm:"primaryKey"`
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primaryKey"`
 	Name      string    `json:"name" gorm:"not null"`
 	NickName  string    `json:"nickname"`
 	Email     string    `json:"email" gorm:"not null"`
